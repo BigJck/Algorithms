ce51954b8ff87a47b1b29f1a622404b7af69630e
BigJck <nikita@lixingrongdeMacBook-Air.local>
2020-03-08 20:53:25 +0800

    Passed

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,21 +1,25 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import edu.princeton.cs.algs4.QuickUnionUF;
-
-import java.util.Random;
 
 public class Percolation {
     private int[] open;
     private int count;
     private int num;
-    private QuickUnionUF quickU;
+    private WeightedQuickUnionUF quickU;
+    //private WeightedQuickUnionUF quickO;
 
     public Percolation(int n) {
-        count = n*n;
+        if (n < 1)
+            throw new IllegalArgumentException();
+        count = n * n;
         num = n;
-        quickU = new QuickUnionUF(count);
+        quickU = new WeightedQuickUnionUF(count);
 
+        //quickO = new WeightedQuickUnionUF(count);
         open = new int[count];
-        for(int i=0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             open[i] = 0;
         }
 
@@ -23,76 +27,104 @@ public class Percolation {
 
     public void open(int row, int col) {
         validate(row, col);
-        if (!isOpen(row, col)){
-            System.out.printf("(%d,%d) is open",row, col);
-            open[row*num+col] = 1;
-            if (row == 0){
-                quickU.union(row*num+col,0);
-                if(isOpen(row+1,col)){
-                    quickU.union((row+1)*num+col,row*num+col);
-                }
+        if (!isOpen(row, col)) {
+            open[(row - 1) * num + (col - 1)] = 1;
+            if (num == 1) {
+                open[(row - 1) * num + (col - 1)] = 2;
+                quickU.union(0, 0);
             }
-            else if (row == num-1){
-                quickU.union(row*num+col,count-1);
-                if(isOpen(row-1,col)){
-                    quickU.union(row*num+col, (row-1)*num+col);
+            else if (row == 1) {
+                quickU.union((row - 1) * num + (col - 1), 0);
+                if (isOpen(row + 1, col)) {
+                    quickU.union(row * num + (col - 1), (row - 1) * num + (col - 1));
+                    open[row * num + (col - 1)] = 2;
+                }
+                open[(row - 1) * num + (col - 1)] = 2;
+            }
+            else if (row == num) {
+                quickU.union((row - 1) * num + (col - 1), count - 1);
+                if (isOpen(row - 1, col)) {
+                    quickU.union((row - 1) * num + (col - 1), (row - 2) * num + (col - 1));
                 }
             }
             else {
-                if (col != 0) {
+                if (col != 1) {
                     if (isOpen(row, col - 1)) {
-                        quickU.union(row * num + col, row * num + col - 1);
+                        quickU.union((row - 1) * num + (col - 1), (row - 1) * num + (col - 2));
                     }
                 }
-                if (col != num - 1) {
+                if (col != num) {
                     if (isOpen(row, col + 1)) {
-                        quickU.union(row * num + col, row * num + col + 1);
+                        quickU.union((row - 1) * num + (col - 1), (row - 1) * num + col);
                     }
                 }
                 if (isOpen(row + 1, col)) {
-                    quickU.union(row * num + col, (row + 1) * num + col);
+                    quickU.union((row - 1) * num + (col - 1), row * num + (col - 1));
                 }
                 if (isOpen(row - 1, col)) {
-                    quickU.union(row * num + col, (row - 1) * num + col);
+                    quickU.union((row - 1) * num + (col - 1), (row - 2) * num + (col - 1));
                 }
             }
         }
     }
 
     public boolean isOpen(int row, int col) {
-        int n = row * num +col;
+        validate(row, col);
+        int n = (row - 1) * num + (col - 1);
         return (open[n] != 0);
     }
 
-    public boolean isFull(int row, int col){
+    public boolean isFull(int row, int col) {
         validate(row, col);
-        if (!isOpen(row, col)){
+        if (open[(row - 1) * num + (col - 1)] == 0) {
             return false;
         }
-        return quickU.connected(row*num+col, 0);
+
+        if (open[(row - 1) * num + (col - 1)] == 1) {
+
+        }
+        return quickU.connected((row - 1) * num + (col - 1), 0);
     }
 
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         int openNum = 0;
-        for(int i = 0; i < count; i++){
-            if(open[i] != 0){
+        for (int i = 0; i < count; i++) {
+            if (open[i] != 0) {
                 openNum++;
             }
         }
         return openNum;
     }
 
-    public boolean percolate(){
-        return quickU.connected(0, count-1);
+    public boolean percolates() {
+        return quickU.connected(0, count - 1);
     }
 
-    public void validate(int row, int col){
-        if(row < 0 || row > count || col < 0 || col > count){
-            throw new IllegalArgumentException("out of range");
+    private void validate(int row, int col) {
+        if (row < 1 || row > num || col < 1 || col > num) {
+            throw new IllegalArgumentException();
         }
     }
 
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Percolation perc = new Percolation(N);
+        boolean percolates = false;
+        int count = 0;
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            if (!perc.isOpen(i, j)) {
+                ++count;
+            }
+            perc.open(i, j);
+            percolates = perc.percolates();
+            if (percolates) break;
+        }
+        StdOut.println(count + " open sites");
+        if (percolates)
+            StdOut.println("percolates");
+        else StdOut.println("does not percolate");
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,70 +1,74 @@
-import java.util.Random;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 
 public class PercolationStats {
     private double[] data;
-    private Percolation[] test;
-    private int T;
+    private int trialsNum;
+    private double meanTemp;
+    private double stddevTemp;
 
-    public PercolationStats(int n, int trials){
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException();
+        }
         data = new double[trials];
-        test = new Percolation[trials];
-        Random rd = new Random();
-        T = trials;
+        Percolation[] test = new Percolation[trials];
+        trialsNum = trials;
 
-        for(int i=0; i<trials; i++){
+        for (int i = 0; i < trials; i++) {
             test[i] = new Percolation(n);
 
-            while (!test[i].percolate()){
-                int x1 = rd.nextInt(n);
-                int x2 = rd.nextInt(n);
-                test[i].open(x1,x2);
+            while (!test[i].percolates()) {
+                int x1 = StdRandom.uniform(n) + 1;
+                int x2 = StdRandom.uniform(n) + 1;
+                test[i].open(x1, x2);
             }
-            data[i] = (double) test[i].numberOfOpenSites() / (n*n);
+            data[i] = (double) test[i].numberOfOpenSites() / (n * n);
         }
-        System.out.println("mean = " + mean());
-        System.out.println("stddev = " + stddev());
-        System.out.println("95% confidence interval = [ " + confidenceLo()+ "," + confidenceHi() + "]");
 
-    }
-
-    public double mean(){
         double temp = 0;
-        for(int i=0; i<T; i++){
+        for (int i = 0; i < trialsNum; i++) {
             temp += data[i];
         }
-        return temp/T;
-    }
+        meanTemp = temp / trialsNum;
 
-    public double stddev(){
-        double square = 0;
+        double square;
         double squareAdd = 0;
-        double meanTemp = mean();
-
-        for(int i=0; i<T; i++){
-            square = (data[i]-meanTemp)*(data[i]-meanTemp);
+        for (int i = 0; i < trialsNum; i++) {
+            square = (data[i] - meanTemp) * (data[i] - meanTemp);
             squareAdd += square;
         }
-        return Math.sqrt(squareAdd / (T-1));
+        stddevTemp = Math.sqrt(squareAdd / (trialsNum - 1));
+
+
     }
 
-    public double confidenceLo(){
-        double sqrt1 = stddev();
-        double sqrt2 = Math.sqrt(T);
-        return mean() - (1.96 * sqrt1 / sqrt2);
+    public double mean() {
+        return meanTemp;
+    }
+
+    public double stddev() {
+        return stddevTemp;
+    }
+
+    public double confidenceLo() {
+        double sqrt2 = Math.sqrt(trialsNum);
+        return mean() - (1.96 * stddevTemp / sqrt2);
     }
 
     public double confidenceHi() {
-        double sqrt1 = stddev();
-        double sqrt2 = Math.sqrt(T);
-        return mean() + (1.96 * sqrt1 / sqrt2);
+        double sqrt2 = Math.sqrt(trialsNum);
+        return mean() + (1.96 * stddevTemp / sqrt2);
     }
 
     public static void main(String[] args) {
-
-
-
-
+        int iN = Integer.parseInt(args[0]), iT = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(iN, iT);
+        StdOut.println("mean                    = " + stats.mean());
+        StdOut.println("stddev                  = " + stats.stddev());
+        StdOut.println("95% confidence interval = " + stats.confidenceLo()
+                               + ", " + stats.confidenceHi());
     }
 }
 
