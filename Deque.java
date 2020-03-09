bb3cd15ce975a88b885f7d309e7229a08b240801
BigJck <997406763@qq.com>
2020-03-09 23:58:35 +0800

    Deque  tested

--- Deque.java
+++ Deque.java
@@ -1,8 +1,5 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -22,14 +19,25 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public boolean isEmpty() {
-        return (first == null || last == null)
+        return (first == null || last == null);
     }
 
     public int size() {
-
+        if (isEmpty()) return 0;
+        else if (first.equals(last)) return 1;
+        else {
+            Node temp = first;
+            int i = 2;
+            while (!temp.next.equals(last)) {
+                temp = temp.next;
+                i++;
+            }
+            return i;
+        }
     }
 
     public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
         Node oldLast = last;
         last = new Node();
         last.item = item;
@@ -39,29 +47,38 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
         Node oldFirst = first;
         first = new Node();
         first.item = item;
         first.next = null;
-        if (isEmpty()) first = last;
+        if (isEmpty()) last = first;
         else first.next = oldFirst;
     }
 
     public Item removeFirst() {
         if (isEmpty()) throw new NoSuchElementException();
-        else first = first.next;
-        if (isEmpty()) first = last;
-
+        Node temp = first;
+        first = first.next;
+        if (isEmpty()) last = first;
+        return temp.item;
     }
 
     public Item removeLast() {
         if (isEmpty()) throw new NoSuchElementException();
         Node temp = first;
-        if (temp.next == null) first = last = null;
-        do {
+        if (first.equals(last)) {
+            last = null;
+            first = null;
+            return temp.item;
+        }
+        while (!temp.next.equals(last)) {
             temp = temp.next;
-        } while (temp.next.equals(last));
-
+        }
+        Node oldLast = last;
+        last = temp;
+        last.next = null;
+        return oldLast.item;
     }
 
     public Iterator<Item> iterator() {
@@ -79,13 +96,42 @@ public class Deque<Item> implements Iterable<Item> {
         public Item next() {
             if (hasNext()) {
                 current = current.next;
+                return current.item;
             }
             else throw new NoSuchElementException("There is no next item");
         }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
     }
 
-
     public static void main(String[] args) {
+        In in = new In(args[0]);
+        Deque<String> deque = new Deque<String>();
+        deque.addFirst(in.readString());
+        deque.addFirst(in.readString());
+        deque.addLast(in.readString());
+        deque.addLast(in.readString());
+        deque.addLast(in.readString());
+
+        // StdOut.printf("Iterator reads " + deque.iterator().next() + "\n");
+        // StdOut.printf("Iterator reads " + deque.iterator().next() + "\n");
+        // StdOut.printf("Iterator reads " + deque.iterator().next() + "\n");
+        // StdOut.printf("Iterator reads " + deque.iterator().next() + "\n");
+        // StdOut.printf("Iterator reads " + deque.iterator().next() + "\n");
+        Iterator<String> i = deque.iterator();
+
+        while (i.hasNext()) {
+            StdOut.printf("iterator reads : " + i.next() + "\n");
+        }
+        StdOut.printf("remove first: " + deque.removeFirst() + '\n');
+        StdOut.printf("remove first: " + deque.removeFirst() + '\n');
+        StdOut.printf("remove last: " + deque.removeLast() + '\n');
+        StdOut.printf("remove last: " + deque.removeLast() + '\n');
+        StdOut.printf("remove last: " + deque.removeLast() + '\n');
+
 
     }
 }
---
+++ RandomizedQueue.java
@@ -0,0 +1,52 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable {
+    public RandomizedQueue() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item sample() {
+
+    }
+
+    public Iterator<Item> iterator() {
+        return new RandomizedIterator;
+    }
+
+    public class RandomizedIterator implements Iterator<Item> {
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Item next() {
+
+        }
+
+        public void remove() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
